3866110e767191bf6424c4f98dc8a5f6e1efa66e
kangkyung <rudgns0508@naver.com>
2022-01-20 07:28:49 +0900

    Project Setting

---
+++ Project/View/TestDrivenDevelopmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestDrivenDevelopmentApp.swift
+//  TestDrivenDevelopment
+//
+//  Created by 강경 on 2022/01/15.
+//
+
+import SwiftUI
+
+@main
+struct TestDrivenDevelopmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
